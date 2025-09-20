84036bab9755467fee4fdaaab4a32b10b103b134
badalkr2004 <badalkr279@gmail.com>
2025-09-21 01:34:31 +0530

    init socket io

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,16 @@ import express from "express";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected".socket.id);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 server.listen(4600, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log("Server running at http://localhost:4600");
 });
