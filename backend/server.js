dca4fb51ef5d3e1d5cd313ecb711819ebf312a24
badalkr2004 <badalkr279@gmail.com>
2025-09-21 01:44:54 +0530

    fix: socket io cors

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import { createServer } from "node:http";
 import { Server } from "socket.io";
-``;
+import cors from "cors";
 import express from "express";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
 
+app.use(cors());
 io.on("connection", (socket) => {
-  console.log("A user connected".socket.id);
+  console.log("A user connected", socket.id);
 });
 
 app.get("/", (req, res) => {
